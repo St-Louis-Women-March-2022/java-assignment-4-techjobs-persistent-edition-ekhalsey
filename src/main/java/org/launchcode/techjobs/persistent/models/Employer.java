bc57d5b679bda40854437ead1d5f2d05467649d4
EmmaHalsey <ekhalsey@yahoo.com>
2022-11-16 20:33:55 -0600

    started part 3

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,14 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
     @NotBlank
     @Size(max = 50, message = "Location must be less than 50 characters")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name="EMPL_ID")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer() {}
 
